ca8fc5b7f9ce129794c5df92bb4e12a6d775e2dc
Joe Bujok <joe.bujok@puntersouthall.com>
2016-10-11 23:44:41 +0100

    getting user listing from database

--- app/src/main/java/com/bujok/sharelocation/MainActivity.java
+++ app/src/main/java/com/bujok/sharelocation/MainActivity.java
@@ -20,11 +20,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.bujok.sharelocation.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.bujok.sharelocation.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 
 public class  MainActivity extends BaseActivity {
@@ -33,14 +45,47 @@ public class  MainActivity extends BaseActivity {
 
     private FragmentPagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
+    private FirebaseDatabase mDatabaseReference;
+    private DatabaseReference mUserDBRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabaseReference = FirebaseDatabase.getInstance();
+        mUserDBRef = mDatabaseReference.getReference("users");
 
+        // Button launches NewPostActivity
+        findViewById(R.id.fab_new_post).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserListing();;
+            }
+        });
+    }
+
+    private void getUserListing() {
+
+        mUserDBRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e("Count " ,""+dataSnapshot.getChildrenCount());
+                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    User u = userSnapshot.getValue(User.class);
+                    Log.e(TAG, u.username);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "reading from the database failed....", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
