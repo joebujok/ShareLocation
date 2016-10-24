6635b0e6fb8b820231bb4c865d6d504fc9f02d15
Joe Bujok <joe.bujok@puntersouthall.com>
2016-10-24 19:56:17 +0100

    facebook integration

--- app/src/main/java/com/bujok/sharelocation/SignInActivity.java
+++ app/src/main/java/com/bujok/sharelocation/SignInActivity.java
@@ -10,11 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.DefaultAudience;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +34,8 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
 
     private static final String TAG = "SignInActivity";
 
+    private static final int PICK_PERMS_REQUEST = 0;
+
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
 
@@ -33,12 +43,17 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
     private EditText mPasswordField;
     private Button mSignInButton;
     private Button mSignUpButton;
+    private LoginButton loginButton;
+
 
     private CallbackManager callbackManager;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
 
@@ -57,19 +72,87 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         mSignInButton.setOnClickListener(this);
         mSignUpButton.setOnClickListener(this);
 
+        loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
 
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
+        // ...
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        // ...
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
+        mAuth.addAuthStateListener(mAuthListener);
         // Check auth on Activity start
         if (mAuth.getCurrentUser() != null) {
             onAuthSuccess(mAuth.getCurrentUser());
         }
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
     private void signIn() {
         Log.d(TAG, "signIn");
         if (!validateForm()) {
@@ -178,5 +261,56 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         } else if (i == R.id.button_sign_up) {
             signUp();
         }
+    }
+    @Override
+    protected void onActivityResult(
+            final int requestCode,
+            final int resultCode,
+            final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,
+                resultCode, data);
+
+/*        if (requestCode == PICK_PERMS_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String[] readPermsArr = data
+                        .getStringArrayExtra(PermissionSelectActivity.EXTRA_SELECTED_READ_PARAMS);
+                String writePrivacy = data
+                        .getStringExtra(PermissionSelectActivity.EXTRA_SELECTED_WRITE_PRIVACY);
+                String[] publishPermsArr = data
+                        .getStringArrayExtra(
+                                PermissionSelectActivity.EXTRA_SELECTED_PUBLISH_PARAMS);
+
+                loginButton.clearPermissions();
+
+                if (readPermsArr != null) {
+                    if (readPermsArr.length > 0) {
+                        loginButton.setReadPermissions(readPermsArr);
+                    }
+                }
+
+                if ((readPermsArr == null ||
+                        readPermsArr.length == 0) &&
+                        publishPermsArr != null) {
+                    if (publishPermsArr.length > 0) {
+                        loginButton.setPublishPermissions(publishPermsArr);
+                    }
+                }
+                // Set write privacy for the user
+                if ((writePrivacy != null)) {
+                    DefaultAudience audience;
+                    if (DefaultAudience.EVERYONE.toString().equals(writePrivacy)) {
+                        audience = DefaultAudience.EVERYONE;
+                    } else if (DefaultAudience.FRIENDS.toString().equals(writePrivacy)) {
+                        audience = DefaultAudience.FRIENDS;
+                    } else {
+                        audience = DefaultAudience.ONLY_ME;
+                    }
+                    loginButton.setDefaultAudience(audience);
+                }
+            }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }*/
     }
 }
